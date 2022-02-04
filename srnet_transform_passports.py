29bf2730b7ca85306b2e6f4d53837ba762aed44b
Masha <maverina518@gmail.com>
2022-02-05 00:14:49 +0300

    Code refactorong. Names and comments.

--- srnet_transform_passports.py
+++ srnet_transform_passports.py
@@ -45,12 +45,12 @@ def create_text_on_gray(text: str, shape: tuple) -> np.ndarray:
     return render_standard_text.make_standard_text(FONT_NAME, text, shape)
 
 
-def create_srnet_styled_text(changeable_image: np.ndarray, standard_text: np.ndarray) -> Image:
+def create_srnet_styled_text(original_image: np.ndarray, skeleton_text: np.ndarray) -> Image:
     """
-    This function styles the text as changeable_image (styled text rendering on background image).
+    This function styles the text as original_image (styled text rendering on background image).
 
-    changeable_image: original image.
-    standard_text: new text skeleton on gray background.
+    original_image: original image.
+    skeleton_text: new text skeleton on gray background.
 
     return: new styled image (o_f from SRNet).
     """
@@ -60,30 +60,30 @@ def create_srnet_styled_text(changeable_image: np.ndarray, standard_text: np.nda
         with tf.Session() as sess:
             saver = tf.train.Saver(tf.global_variables())
             saver.restore(sess, args.model_path)
-            styled_text = model.predict(sess, standard_text, changeable_image)[-1]
+            styled_text = model.predict(sess, skeleton_text, original_image)[-1]
 
     return Image.fromarray(styled_text)
 
 
-def create_styled_img(text: str, changeable_image: Image, shape: tuple) -> Image:
+def create_styled_img(text: str, original_image: Image) -> Image:
     """
     This function changes the image text(o_f from SRNet).
     text: new text of entity.
-    changeable_image: cut out image.
+    original_image: cut out image.
     shape: shape of new img
 
     return: cutout image of the entity.
     """
+    original_image = np.array(original_image)
+    skeleton_text = create_text_on_gray(text=text, shape=(original_image.shape[0], original_image.shape[1]))
 
-    standard_text = create_text_on_gray(text, shape)
-
-    changeable_image = np.array(changeable_image)
+    original_image = np.array(original_image)
     # normalize the image in the range from -1 to 1.
-    standard_text = np.resize(standard_text, changeable_image.shape).astype(np.float32) / 127.5 - 1.0
-    changeable_image = np.resize(changeable_image, changeable_image.shape).astype(np.float32) / 127.5 - 1.0
+    skeleton_text = np.resize(skeleton_text, original_image.shape).astype(np.float32) / 127.5 - 1.0
+    original_image = np.resize(original_image, original_image.shape).astype(np.float32) / 127.5 - 1.0
 
     # predict o_f
-    styled_text = create_srnet_styled_text(changeable_image, standard_text)
+    styled_text = create_srnet_styled_text(original_image=original_image, skeleton_text=skeleton_text)
 
     return styled_text
 
@@ -94,6 +94,7 @@ def generate_path(input_path: str, output_path: str) -> None:
 
     """
     Path(output_path).mkdir(parents=True, exist_ok=True)
+
     for file in tqdm(os.listdir(input_path)):
         if Path(file).suffix in IMAGE_FORMATS:
             json_file = Path(input_path) / Path(file).with_suffix('.json')
@@ -106,22 +107,22 @@ def generate_path(input_path: str, output_path: str) -> None:
             passport_content.random_init()
 
             for label_elem in markup['shapes']:
-                if not label_elem['label'] in ['officer_signature', 'signature', 'photo', 'passport']:
-                    x_0, y_0, x_1, y_1 = int(label_elem['points'][0][0]), int(label_elem['points'][0][1]), int(
-                        label_elem['points'][2][0]), int(label_elem['points'][2][1])
+                if not label_elem['label'] in ('officer_signature', 'signature', 'photo', 'passport'):
+                    x_0, y_0, x_1, y_1 = int(label_elem['points'][0][0]), int(label_elem['points'][0][1]), \
+                                         int(label_elem['points'][2][0]), int(label_elem['points'][2][1])
                     # i_s from SRNet
-                    changeable_image = img.crop(tuple([x_0, y_0, x_1, y_1]))
-                    if label_elem['label'] in ['number_group1', 'number_group2']:
-                        changeable_image = changeable_image.rotate(90, expand=True)
-                        shape = changeable_image.size
-                    else:
-                        shape = (changeable_image.size[1], changeable_image.size[0])
+                    original_image = img.crop(tuple([x_0, y_0, x_1, y_1]))
+                    rotate = label_elem['label'] in ('number_group1', 'number_group2')
+
+                    if rotate:
+                        original_image = original_image.rotate(90, expand=True)
 
                     text = passport_content.get(label_elem['label'])
 
                     # o_f from SRNet
-                    styled_text = create_styled_img(text, changeable_image, shape)
-                    if label_elem['label'] in ['number_group1', 'number_group2']:
+                    styled_text = create_styled_img(text=text, original_image=original_image)
+
+                    if rotate:
                         styled_text = styled_text.rotate(270, expand=True)
 
                     img.paste(styled_text, (x_0, y_0))
@@ -160,7 +161,7 @@ def init_argparse():
     parser.add_argument(
         '--gpu',
         nargs='?',
-        help='GPU for model. If use cpu gpu is -1.',
+        help='GPU for model. If use cpu, param is -1.',
         default=-1,
         type=int)
     return parser
@@ -170,4 +171,4 @@ if __name__ == "__main__":
     args = init_argparse().parse_args()
     if args.gpu != -1:
         os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
-    generate_path(args.input_path, args.output_path)
+    generate_path(input_path=args.input_path, output_path=args.output_path)
